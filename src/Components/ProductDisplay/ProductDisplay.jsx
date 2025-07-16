c3de3531a1e7fba60de7c1007fb64f2d0a9e0a54
Daniel <156513019+Stuck-in-code@users.noreply.github.com>
2025-07-16 10:45:11 +0000

    Update ProductDisplay.jsx

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,6 +1,6 @@
 import "./ProductDisplay.css";
-import star_icon from "../../Assets/star_icon.png";
-import star_dull_icon from "../../Assets/star_dull_icon.png";
+import star_icon from "../../assets/star_icon.png";
+import star_dull_icon from "../../assets/star_dull_icon.png";
 import { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 
